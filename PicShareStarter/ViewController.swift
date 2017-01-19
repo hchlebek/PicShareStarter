d9ceb20820df9a1b4a7e3b751186bf8fe16a1973
HChlebek <student@hchlebek.local>
2017-01-19 09:44:09 -0600

    Initial Commit

---
+++ PicShareStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PicShareStarter
+//
+//  Created by HChlebek on 1/19/17.
+//  Copyright © 2017 HChlebek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
